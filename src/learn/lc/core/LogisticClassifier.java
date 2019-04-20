f445ce327e5c8b56b3f54676670d55b77568cced
ywang249 <ywang249@u.rochester.edu>
2019-04-19 23:27:16 -0400

    Initial commit

---
+++ src/learn/lc/core/LogisticClassifier.java
@@ -0,0 +1,26 @@
+package learn.lc.core;
+
+import learn.math.util.VectorOps;
+
+public class LogisticClassifier extends LinearClassifier {
+	
+	public LogisticClassifier(int ninputs) {
+		super(ninputs);
+	}
+	
+	/**
+	 * A LogisticClassifier uses the logistic update rule
+	 * (AIMA Eq. 18.8): w_i \leftarrow w_i+\alpha(y-h_w(x)) \times h_w(x)(1-h_w(x)) \times x_i 
+	 */
+	public void update(double[] x, double y, double alpha) {
+		// This must be implemented by you
+	}
+	
+	/**
+	 * A LogisticClassifier uses a 0/1 sigmoid threshold at z=0.
+	 */
+	public double threshold(double z) {
+		// This must be implemented by you
+	}
+
+}
---
+++ src/learn/lc/core/PerceptronClassifier.java
@@ -0,0 +1,26 @@
+package learn.lc.core;
+
+import learn.math.util.VectorOps;
+
+public class PerceptronClassifier extends LinearClassifier {
+	
+	public PerceptronClassifier(int ninputs) {
+		super(ninputs);
+	}
+	
+	/**
+	 * A PerceptronClassifier uses the perceptron learning rule
+	 * (AIMA Eq. 18.7): w_i \leftarrow w_i+\alpha(y-h_w(x)) \times x_i 
+	 */
+	public void update(double[] x, double y, double alpha) {
+		// This must be implemented by you
+	}
+	
+	/**
+	 * A PerceptronClassifier uses a hard 0/1 threshold.
+	 */
+	public double threshold(double z) {
+		// This must be implemented by you
+	}
+	
+}
